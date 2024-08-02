4d5261c382d55171bb09fa9ca1bfe6c85c12270b
SUBHAJIT064 <102734135+SUBHAJIT064@users.noreply.github.com>
2024-08-02 22:22:40 +0530

    Add files via upload

---
+++ IRepo.cs
@@ -0,0 +1,9 @@
+﻿
+namespace JWTAuth.Data.Repositories
+{
+    public interface IRepo<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetAsync(int Id);
+    }
+}
---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using JWTAuth.Common.Helper;
+using JWTAuth.Data.Repositories;
+using JWTAuth.Helpers;
+using JWTAuth.Services;
+using Npgsql;
+using System.Data;
+
+namespace JWTAuth.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void UseOneTransactionPerHttpCall(this IServiceCollection serviceCollection, AppSettings _appSettings, IsolationLevel level = IsolationLevel.ReadUncommitted)
+        {
+
+            serviceCollection.AddScoped<IDbTransaction>(serviceProvider =>
+            {
+                var connectionStringProvider = serviceProvider.GetService<IConnectionStringProvider>();
+                var connection = new NpgsqlConnection(_appSettings.ConnectionString);
+
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                return connection.BeginTransaction(level);
+            });
+
+            serviceCollection.AddScoped(typeof(UnitOfWorkFilter), typeof(UnitOfWorkFilter));
+
+            serviceCollection.AddMvc(setup =>
+            {
+                setup.Filters.AddService<UnitOfWorkFilter>(1);
+            });
+        }
+    }
+}
